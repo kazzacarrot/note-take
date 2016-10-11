cd841f412fc172a26852ff3f116f5ec17dbe40c3
Karen McCulloch <k.a.z.mcc95@gmail.com>
2016-10-12 00:18:14 +0100

    add export and saving functions

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 console.log("hollo");
 
-function retrieveData(){
-
-    var s = sessionStorage.getItem('note');
-    return s;
-}
 
 function checkCompatibility(){
     if (typeof(Storage) !== "undefined") {
@@ -27,48 +22,39 @@ function clearNotesElement(){
     }
 }
 
+
 function display(){
+    console.log("clearing");
     clearNotesElement();
-    var o, para, t;
-    for (n in notes){
-        o = JSON.stringify(n + " : " + notes[n]);
-        para = document.createElement("P");
+
+    console.log("setting up");
+    var o, heading, t;
+    console.log(notenotes);
+    for (n in notenotes){
+        o = (notenotes[n].heading).toString();
+
+
+        heading = document.createElement("H3");
         t = document.createTextNode(o);
-        para.appendChild(t);
-        document.getElementById("notes").appendChild(para);
+        heading.appendChild(t);
+
+        text = document.createElement("P");
+        t = document.createTextNode(notenotes[n].toString());
+        text.appendChild(t);
+
+        div = document.createElement("DIV");
+        div.appendChild(heading);
+        div.appendChild(text);
+
+        document.getElementById("notes").appendChild(div);
     }
 }
 
-function clearData(a, b){
-    notes = {}
-    sessionStorage.setItem('note', {});
-    display();
-}
-function saveData(a, b){
-    notes[a] = b;
-    display();
-}
-function saveNotes(a, b){
-    dict = retrieveData();
-    console.log(dict);
-    dict.setItem(a.toString, b.toString());
-    sessionStorage.setItem('note', JSON.stringify(dict));
-    alert("data saved to local storage.");
-    console.log("data saved.");
-}
 
-// initalize note 
-var notes = {h : "w"};
 
 function startApp(){
     if (checkCompatibility()){
-        s = retrieveData();
-        console.log(typeof (s));
-
-        if (s != null){
-            // initalize notes variable with the value in storage.
-           notes = JSON.parse(s);
-        }
+        s = note.retrieveDataFromLocalStorage();
     }
     // display the value of notes
     display();
---
+++ model.js
@@ -0,0 +1,116 @@
+var notenotes = [];
+class note {
+    constructor(heading, description){
+        console.log(heading);
+        console.log(description);
+        this.heading      = heading;
+        this.description  = description;
+        this.lastModified = new Date();
+    }
+
+    edit(h, d){
+        this.heading      = h;
+        this.description  = d;
+        this.lastModified = new Date();
+    }
+    changeName(h){
+        this.heading = h;
+        this.lastModified = new Date();
+    }
+    removeNote(){
+        delete this;
+        var i = notenotes.indexOf(this);
+        notenotes.splice(index, 1);
+    }
+    static query(date, heading, content){
+        for (n in notenotes){
+            console.log(notenotes[n]);
+        }
+    }
+    static retrieveDataFromLocalStorage(){
+        var s = localStorage.getItem('note');
+        console.log(typeof (s));
+        if (s != null){
+
+            var notes = JSON.parse(s);
+            console.log("S IS NOT NULL");
+        }
+        for (var n in notes){
+            console.log("What is this");
+            console.log(notes);
+            console.log("IT IS:");
+            console.log(notes[n]);
+            var o = new note(notes[n].heading, notes[n].description);
+            notenotes.push(o);
+        }
+
+        display()
+    }
+
+    toString(){
+        return this.description + " @ " + this.lastModified
+    }
+    static exportDataToCSV(){
+        var data =[];
+        data.push([" ", "Heading", "Description", "Time"]);
+        notenotes.forEach(function (note, index){
+            data.push([note.heading, note.description, note.lastModified])
+        })
+        console.log(data);
+        var csvContent = "data:text/csv;charset=utf-8";
+        var dataString;
+        data.forEach(function(infoArray, index) {
+            dataString = infoArray.join(",");
+            csvContent += index < data.length ? dataString + "\n": dataString; 
+
+        })
+        console.log(dataString);
+        console.log(csvContent);
+        var encodedUri = encodeURI(csvContent);
+        window.open(encodedUri);
+    }
+
+    static saveDataToLocalStorage(){
+        localStorage.setItem('note', JSON.stringify(notenotes));
+
+        console.log("data saved.");
+    }
+    static clearData(){
+        console.log("clearing data from display");
+        notenotes = [];
+        console.log("clearing data from local storage");
+        localStorage.setItem('note', {});
+    }
+}
+
+
+function exportData(){
+    console.log("exporting");
+    note.exportDataToCSV();
+}
+
+function saveData(a, b){
+        c = typeof a.value !== "undefined" ? a.value : "Note Heading";
+    d = typeof b.value !== "undefined" ? b.value : "Note Content"; 
+
+    console.log("saving");
+    var newNote = new note(c, d);
+
+    console.log("LKJO");
+    console.log(newNote);
+    notenotes.push(newNote);
+    display();
+}
+
+function clearData(){
+    note.clearData();
+    display();
+}
+
+
+function queryData(date, keyword, keyword){
+    date = typeof date.value !== "undefined" ? date.value : "Note Heading";
+    keyword = typeof keyword.value !== "undefined" ? keyword.value : "Note Content"; 
+    note.query(date, keyword);
+}
+
